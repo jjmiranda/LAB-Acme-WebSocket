8f9e438e76ab325169ab5800c271cb57a4d63130
Juan Jose Miranda <jjmiranda@magiadigital.com>
2023-01-02 21:35:11 -0500

    Eventos actualiados

--- index.ts
+++ index.ts
@@ -17,11 +17,25 @@ class App {
     private pipes(): void { 
         logger.info(`Server is running at https://localhost:${port}`);
         this.io.on("connection", (socket) => {
+            socket.on("shared-identity-custom", (args) => {
+                logger.info(`Emitiendo un shared-identity-custom-client`);
+                socket.to(args.to).emit('shared-identity-custom-client', args.content);
+            });
+            socket.on("shared-identity-custom-2", (args) => {
+                logger.info(`Emitiendo un shared-identity-custom-2-client`);
+                socket.to(args.to).emit('shared-identity-custom-2-client', args.content);
+            });
             socket.on("shared-identity", (args) => {
+                logger.info(`Emitiendo un shared-identity-client`);
                 socket.to(args.to).emit('shared-identity-client', args.content);
             });
             socket.on("shared-credential", (args) => {
+                logger.info(`Emitiendo un shared-credential-client`);
                 socket.to(args.to).emit('shared-credential-client', args.content);
+            });
+            socket.on("vc-downloaded", (args) => {
+                logger.info(`Emitiendo un vc-downloaded-client`);
+                socket.to(args.to).emit('vc-downloaded-client', args.content);
             });
             socket.on("access-history", (args) => {
                 socket.broadcast.emit('access-history-client', args.content);
